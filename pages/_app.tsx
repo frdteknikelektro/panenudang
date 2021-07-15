6603a3b8eb3ae59cd046beb06596abaebcc60e4c
syauqy <syauqyaziz@gmail.com>
2021-07-15 21:11:14 +0700

    fixing _app page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,10 @@
+import * as React from "react";
 import "../styles/globals.css";
 import "../styles/storymodal.css";
+import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider>
       <Component {...pageProps} />
